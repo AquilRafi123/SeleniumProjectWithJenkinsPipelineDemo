adf1663d1ed93a3a6c497c395b5f2cf374383ec8
AquilRafi123 <nahidnahid365@gmail.com>
2025-11-11 23:29:00 +0530

    newPomandTestNgUpdate

--- src/test/java/FirstTestCase.java
+++ src/test/java/FirstTestCase.java
@@ -1,47 +1,33 @@
-
-
-
-/*Test Case
- *1)LAunch Browser(Chrome)
- *2)Open URL: https://demo.opencart.com/
- *3)Validate title should be "Your Store"
- *4)Close Browser 
- * */
-
-
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.WebDriver; // this is added manually 
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.annotations.Test;
+import org.openqa.selenium.WebDriver;
 
 public class FirstTestCase {
 
-	public static void main(String[] args) {
-		
-		// 1)LAunch Browser(Chrome)---
-		// we create  an object for ChromeDriver class  because we need chrome launch , and 
-		// as soon as we create object ,that ChromeDriver class has an constructor which launches chrome browser
-		
-//		ChromeDriver driver= new ChromeDriver();
-		WebDriver driver= new ChromeDriver();  // upcasting // either is fine // this is preferred 
-		
-		//2)Open URL: https://demo.opencart.com/
-		driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
-		
-		//3)Validate title should be "Your STore"
-		
-		String act_title=driver.getTitle();
-		if(act_title.equalsIgnoreCase("OrangeHRM"))
-		{
-			System.out.println("Test Passed");
-		}
-		else
-		{
-			System.out.println("Test Failed ");
-		}
-		
-		//4)Close Browser 
-		//driver.close();
-		driver.quit();
+    @Test
+    public void loginTest() {
 
-	}
+        WebDriverManager.chromedriver().setup();
+        
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--headless"); 
+        options.addArguments("--disable-gpu");
+        
+        WebDriver driver = new ChromeDriver(options);
 
+        driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
+        
+        String title = driver.getTitle();
+        System.out.println("Page Title: " + title);
+
+        if (title.contains("OrangeHRM")) {
+            System.out.println("Test Passed!");
+        } else {
+            System.out.println("Test Failed!");
+        }
+
+        driver.quit();
+    }
 }
